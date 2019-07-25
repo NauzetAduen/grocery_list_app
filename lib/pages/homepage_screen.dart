ab2930579317b3c3276f2516f723ed9458768561
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-25 18:34:57 +0100

    CHANGE homepage files

--- lib/pages/homepage_screen.dart
+++ lib/pages/homepage_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("HomePage"),),
+      appBar: AppBar(
+        title: Text("HomePage"),
+      ),
       body: Center(child: Text("HomePage")),
     );
   }
