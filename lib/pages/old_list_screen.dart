2f551766fe40229c8d995b1d48ddfa0aeb9c9c52
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-25 18:34:40 +0100

    ADD simple old list screen

---
+++ lib/pages/old_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OldListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("OldList"),
+      ),
+      body: Center(
+        child: Text("OLD LIST"),
+      ),
+    );
+  }
+}
