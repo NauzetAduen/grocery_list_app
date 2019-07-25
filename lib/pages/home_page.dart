1d9a3e24207cca27e614323dac4530fd4325f6e5
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-25 17:38:47 +0100

    ADD simple home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("HomePage"),),
+      body: Center(child: Text("HomePage")),
+    );
+  }
+}
