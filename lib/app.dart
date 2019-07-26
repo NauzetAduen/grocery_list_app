32d357f8fc3bf33c06dee7f09622d18b4c70e292
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-26 17:56:55 +0100

    CHANGE to new bottomnavbar

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,8 @@
-import 'package:circular_bottom_navigation/circular_bottom_navigation.dart';
-import 'package:circular_bottom_navigation/tab_item.dart';
 import 'package:flutter/material.dart';
 import 'package:grocery_list_app/pages/homepage_screen.dart';
 import 'package:grocery_list_app/pages/old_list_screen.dart';
 import 'package:grocery_list_app/pages/products_screen.dart';
+import 'package:titled_navigation_bar/titled_navigation_bar.dart';
 
 class App extends StatefulWidget {
   @override
@@ -12,12 +11,10 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   int _currentIndex = 0;
-  CircularBottomNavigationController _navigationController;
 
   @override
   void initState() {
     super.initState();
-    _navigationController = CircularBottomNavigationController(_currentIndex);
   }
 
   @override
@@ -25,18 +22,19 @@ class _AppState extends State<App> {
     return Scaffold(
       body: [HomePageScreen(), OldListScreen(), ProductsScreen()]
           .elementAt(_currentIndex),
-      bottomNavigationBar: CircularBottomNavigation(
-        List.of([
-          TabItem(Icons.home, "Home", Colors.blue),
-          TabItem(Icons.list, "Old lists", Colors.orange),
-          TabItem(Icons.layers, "Products", Colors.red),
-        ]),
-        controller: _navigationController,
-        selectedCallback: (int selectedPos) {
-          setState(
-              () => _currentIndex = _navigationController.value = selectedPos);
-        },
-      ),
+      bottomNavigationBar: TitledBottomNavigationBar(
+          // reverse: true,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          currentIndex: _currentIndex,
+          items: [
+            TitledNavigationBarItem(title: "Home", icon: Icons.home),
+            TitledNavigationBarItem(title: "Old lists", icon: Icons.list),
+            TitledNavigationBarItem(title: "Products", icon: Icons.layers)
+          ]),
     );
   }
 }
