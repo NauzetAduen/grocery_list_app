314d8580b1d8ed3220591560a06547db6bd96696
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-26 14:21:18 +0100

    ADD alert dialog

--- lib/components/product_grid_view.dart
+++ lib/components/product_grid_view.dart
@@ -7,14 +7,40 @@ class ProductGridView extends StatelessWidget {
   const ProductGridView({Key key, this.document}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.red,
-      child: Center(
-        child: Text(
-          "${document.data}",
-          style: TextStyle(fontSize: 25),
+    String name = document['name'];
+    return GestureDetector(
+      onTap: () => _showDialog(context, name),
+      child: Card(
+        color: Colors.red,
+        child: Center(
+          child: Text(
+            name,
+            style: TextStyle(fontSize: 22),
+          ),
         ),
       ),
     );
+  }
+
+  _showDialog(BuildContext context, String name) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(
+              name,
+              textAlign: TextAlign.center,
+            ),
+            content: Container(
+              width: 300,
+              height: 300,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/example.jpg'))),
+              ),
+            ),
+          );
+        });
   }
 }
