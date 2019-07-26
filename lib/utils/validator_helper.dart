163d59c5fcefdbe7401f748544552431697ecb32
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-26 21:42:22 +0100

    FIX bug with empty messages

--- lib/utils/validator_helper.dart
+++ lib/utils/validator_helper.dart
@@ -1,5 +1,5 @@
 class ValidatorHelper {
   static String newProductValidator(String value) {
-    return value.isEmpty ? "Please add a name" : "";
+    return value.isEmpty ? "Please add a name" : null;
   }
 }
