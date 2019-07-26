2a1ffcb31b2871b87cd844e22cce8ce4a05049a6
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-26 20:45:48 +0100

    CHANGE to curve navbar

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,9 @@
-import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:grocery_list_app/Style/style.dart';
 import 'package:grocery_list_app/pages/homepage_screen.dart';
 import 'package:grocery_list_app/pages/old_list_screen.dart';
 import 'package:grocery_list_app/pages/products_screen.dart';
-import 'package:titled_navigation_bar/titled_navigation_bar.dart';
 
 class App extends StatefulWidget {
   @override
@@ -16,26 +16,49 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: [HomePageScreen(), OldListScreen(), ProductsScreen()]
-            .elementAt(_currentIndex),
-        bottomNavigationBar: BottomNavyBar(
-          selectedIndex: _currentIndex,
-          onItemSelected: (int value) {
-            setState(() {
-              _currentIndex = value;
-            });
-          },
-          items: [
-            BottomNavyBarItem(
-                title: Text("Home"),
-                icon: Icon(Icons.home),
-                activeColor: Colors.green),
-            BottomNavyBarItem(title: Text("Old lists"), icon: Icon(Icons.list)),
-            BottomNavyBarItem(
-                title: Text("Products"),
-                icon: Icon(Icons.fastfood),
-                activeColor: Colors.red),
-          ],
-        ));
+      body: [HomePageScreen(), OldListScreen(), ProductsScreen()]
+          .elementAt(_currentIndex),
+      bottomNavigationBar: CurvedNavigationBar(
+        height: 50,
+        backgroundColor: Colors.transparent,
+        color: Style.green,
+        index: _currentIndex,
+        items: <Widget>[
+          Icon(
+            Icons.home,
+          ),
+          Icon(
+            Icons.list,
+          ),
+          Icon(
+            Icons.fastfood,
+          ),
+        ],
+        onTap: (newIndex) {
+          setState(() {
+            _currentIndex = newIndex;
+          });
+        },
+      ),
+    );
+    // bottomNavigationBar: BottomNavyBar(
+    //   selectedIndex: _currentIndex,
+    //   onItemSelected: (int value) {
+    //     setState(() {
+    //       _currentIndex = value;
+    //     });
+    //   },
+    //   items: [
+    //     BottomNavyBarItem(
+    //         title: Text("Home"),
+    //         icon: Icon(Icons.home),
+    //         activeColor: Colors.green),
+    //     BottomNavyBarItem(title: Text("Old lists"), icon: Icon(Icons.list)),
+    //     BottomNavyBarItem(
+    //         title: Text("Products"),
+    //         icon: Icon(Icons.fastfood),
+    //         activeColor: Colors.red),
+    //   ],
+    // ));
   }
 }
