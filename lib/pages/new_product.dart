df277006d5f4e96376d09253588fe606b3b3b784
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-26 21:23:31 +0100

    ADD form to newproduct view

--- lib/pages/new_product.dart
+++ lib/pages/new_product.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_widgets/gradient_widgets.dart';
+import 'package:grocery_list_app/Style/style.dart';
+import 'package:grocery_list_app/components/horizontal_separator.dart';
 import 'package:grocery_list_app/components/leading_appbar.dart';
+import 'package:grocery_list_app/utils/validator_helper.dart';
 
 class NewProduct extends StatefulWidget {
   @override
@@ -7,13 +11,49 @@ class NewProduct extends StatefulWidget {
 }
 
 class _NewProductState extends State<NewProduct> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: LeadingAppbar(Text("New product")),
-      body: Center(
-        child: Text("AAA"),
-      ),
-    );
+        appBar: LeadingAppbar(Text("New product")),
+        body: Container(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Row(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 4,
+                        child: TextFormField(
+                          controller: _controller,
+                          validator: ValidatorHelper.newProductValidator,
+                        ),
+                      ),
+                      HorizontalSeparator(
+                        width: 30,
+                      ),
+                      Hero(
+                        tag: 'newProduct',
+                        child: GradientButton(
+                            child: Icon(Icons.check),
+                            callback: () => _validateNewProduct(),
+                            gradient: LinearGradient(
+                              colors: [Colors.green, Style.green],
+                            )),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
+
+  _validateNewProduct() {}
 }
