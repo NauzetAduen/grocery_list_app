40b72e10da65f8538f79b5bd8b8f6a0291de43e2
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-27 16:02:55 +0100

    ADD actions to create new list

--- lib/pages/homepage_screen.dart
+++ lib/pages/homepage_screen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:grocery_list_app/components/custom_appbar.dart';
 
 class HomePageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppbar(
-        Text("HomePage"),
+      appBar: CustomAppbar(Text("HomePage"), actions: <Widget>[
+        IconButton(
+          onPressed: () => print("FIESHTA"),
+          icon: Icon(FontAwesomeIcons.plus),
+        ),
+      ]),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[],
+        ),
       ),
-      body: Center(child: Text("HomePage")),
     );
   }
 }
