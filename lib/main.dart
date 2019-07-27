10debffc4ea6d8f10392d1cf903ff0ae45b6e54d
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-27 12:16:01 +0100

    ADD streamProvider to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:grocery_list_app/app.dart';
+import 'package:provider/provider.dart';
 
 import 'Style/style.dart';
 
@@ -9,10 +11,13 @@ class MyApp extends StatelessWidget {
   final String appName = "Grocery List App";
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: appName,
-      theme: ThemeData(primaryColor: Style.green),
-      home: App(),
+    return StreamProvider<QuerySnapshot>.value(
+      child: MaterialApp(
+        title: appName,
+        theme: ThemeData(primaryColor: Style.green),
+        home: App(),
+      ),
+      value: Firestore.instance.collection("products").snapshots(),
     );
   }
 }
