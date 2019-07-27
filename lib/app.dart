a343fa9e5248df5b7d7fda4d0ac83eb827864738
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-27 15:39:02 +0100

    ADD static icon size in navbar

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:grocery_list_app/Style/style.dart';
 import 'package:grocery_list_app/pages/homepage_screen.dart';
 import 'package:grocery_list_app/pages/old_list_screen.dart';
@@ -12,6 +13,7 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   int _currentIndex = 0;
+  static const iconSize = 22.0;
 
   @override
   Widget build(BuildContext context) {
@@ -25,13 +27,16 @@ class _AppState extends State<App> {
         index: _currentIndex,
         items: <Widget>[
           Icon(
-            Icons.home,
+            FontAwesomeIcons.user,
+            size: iconSize,
           ),
           Icon(
-            Icons.list,
+            FontAwesomeIcons.list,
+            size: iconSize,
           ),
           Icon(
             Icons.fastfood,
+            size: iconSize,
           ),
         ],
         onTap: (newIndex) {
