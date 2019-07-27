3f70d86f82c528cb08da51d7849981c138414d28
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-27 13:08:33 +0100

    ADD navigator in action menu

--- lib/pages/products_screen.dart
+++ lib/pages/products_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:grocery_list_app/components/custom_appbar.dart';
 import 'package:grocery_list_app/components/product_grid_view.dart';
 import 'package:grocery_list_app/pages/new_product.dart';
@@ -19,9 +20,12 @@ class ProductsScreen extends StatelessWidget {
           QuerySnapshot snap = snapshot.data;
 
           return Scaffold(
-            appBar: CustomAppbar(
-              Text("Products screen"),
-            ),
+            appBar: CustomAppbar(Text("Products screen"), actions: <Widget>[
+              IconButton(
+                onPressed: () => _onPress(context),
+                icon: Icon(FontAwesomeIcons.plus),
+              ),
+            ]),
             body: GridView.builder(
               itemCount: snap.documents.length,
               gridDelegate:
@@ -30,24 +34,19 @@ class ProductsScreen extends StatelessWidget {
                 return ProductGridView(document: snap.documents[index]);
               },
             ),
-            floatingActionButton: FloatingActionButton(
-              heroTag: 'newProduct',
-              child: Icon(Icons.add),
-              onPressed: () async {
-                var result = await Navigator.push(context,
-                    CupertinoPageRoute(builder: (context) => NewProduct()));
-
-                if (result != null) {
-                  Scaffold.of(context).showSnackBar(SnackBar(
-                    content: Text("$result added"),
-                    duration: Duration(milliseconds: 4000),
-                  ));
-                }
-              },
-            ),
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.miniStartTop,
           );
         });
+  }
+
+  _onPress(BuildContext context) async {
+    var result = await Navigator.push(
+        context, CupertinoPageRoute(builder: (context) => NewProduct()));
+
+    if (result != null) {
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text("$result added"),
+        duration: Duration(milliseconds: 4000),
+      ));
+    }
   }
 }
