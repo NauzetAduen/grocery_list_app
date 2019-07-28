40c5bd23d7ee95f5f5558c973e90eaf6e854d25c
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-28 20:40:05 +0100

    CHANGE homePage to statefull

--- lib/pages/homepage_screen.dart
+++ lib/pages/homepage_screen.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:grocery_list_app/components/custom_appbar.dart';
 
-class HomePageScreen extends StatelessWidget {
+class HomePageScreen extends StatefulWidget {
+  @override
+  _HomePageScreenState createState() => _HomePageScreenState();
+}
+
+class _HomePageScreenState extends State<HomePageScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
