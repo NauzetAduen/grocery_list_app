489c1c762783ce701bc2730df69393c2da27180f
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-28 20:39:36 +0100

    ADD basic profile screen

---
+++ lib/pages/profile_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("AAAAA"));
+  }
+}
