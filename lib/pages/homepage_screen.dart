0d761215607026785f56753b250d83315e15c2d2
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-29 17:48:22 +0100

    ADD streambuilder in homepage

--- lib/pages/homepage_screen.dart
+++ lib/pages/homepage_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:grocery_list_app/components/custom_appbar.dart';
+import 'package:grocery_list_app/models/grocery_list.dart';
 import 'package:grocery_list_app/models/item.dart';
-import 'package:grocery_list_app/models/product.dart';
 
 class HomePageScreen extends StatefulWidget {
   @override
@@ -10,73 +13,104 @@ class HomePageScreen extends StatefulWidget {
 }
 
 class _HomePageScreenState extends State<HomePageScreen> {
-  List<Item> list;
-
   @override
   void initState() {
     super.initState();
-    list = List<Item>();
-    list.add(Item("Cocacola", 5, "latas"));
-    list.add(Item("Papas", 2, "Kilos"));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppbar(Text("HomePage"), actions: <Widget>[
-        IconButton(
-          onPressed: () {
-            _checkList();
-          },
-          icon: Icon(FontAwesomeIcons.plus),
-        ),
-      ]),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: list.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(
-                      "${list[index].quantity} ${list[index].magnitude} ${list[index].productName} "),
-                );
-              },
-            )
-          ],
-        ),
-      ),
-    );
+    return StreamBuilder<Object>(
+        stream: Firestore.instance
+            .collection("lists")
+            .where("groupId", isEqualTo: 1)
+            .where("active", isEqualTo: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          QuerySnapshot data = snapshot.data;
+          List<DocumentSnapshot> documents = data.documents;
+
+          return Scaffold(
+            appBar: CustomAppbar(Text("HomePage"), actions: <Widget>[
+              IconButton(
+                onPressed: () {
+                  _checkList();
+                },
+                icon: Icon(FontAwesomeIcons.plus),
+              ),
+            ]),
+            body: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: documents.length,
+                    itemBuilder: (context, index) {
+                      Map<String, dynamic> mapJson = documents[index].data;
+                      GroceryList myList = GroceryList.fromJson(
+                          json.decode(json.encode(mapJson)));
+                      return ListTile(title: Text("risas $myList"));
+                    },
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      print("PRESS");
+                      // _testUpdate(documents[0].documentID);
+                    },
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 
   _checkList() {
-    if (list.isNotEmpty) {
-      showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text("List is not empty"),
-              content: Text("Do you wan't to create a new list?"),
-              actions: <Widget>[
-                FlatButton(
-                  child: Text("Cancel"),
-                  onPressed: () => Navigator.pop(context),
-                ),
-                FlatButton(
-                  child: Text("OK"),
-                  onPressed: () => _createNewList(),
-                ),
-              ],
-            );
-          });
-    }
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("List is not empty"),
+            content: Text("Do you wan't to create a new list?"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Cancel"),
+                onPressed: () => Navigator.pop(context),
+              ),
+              FlatButton(
+                child: Text("OK"),
+                onPressed: () => _createNewList(),
+              ),
+            ],
+          );
+        });
   }
 
   _createNewList() {
-    setState(() {
-      list.clear();
-    });
+    setState(() {});
     Navigator.pop(context);
+  }
+
+  _testUpdate(String documentID) {
+    Firestore.instance.runTransaction((Transaction transaction) async {
+      DocumentReference reference =
+          Firestore.instance.collection("lists").document(documentID);
+      DocumentSnapshot snapshot = await transaction.get(reference);
+      GroceryList a = GroceryList(
+          1,
+          DateTime.now(),
+          [
+            {'productName': 'coca', 'productMagnitude': '3 kilos'},
+            {'productName': 'refresco', 'productMagnitude': '4 latas'},
+            {'productName': 'tomates', 'productMagnitude': '4 kilos'},
+          ],
+          true);
+
+      await transaction.update(snapshot.reference, a.toJson());
+    });
   }
 }
