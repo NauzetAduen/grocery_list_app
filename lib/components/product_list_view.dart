b97f55ff6d8acf5c4b6e57c154b4b87317817df2
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-31 18:44:54 +0100

    ADD temp coloring to listviews

--- lib/components/product_list_view.dart
+++ lib/components/product_list_view.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:grocery_list_app/Style/style.dart';
 import 'package:grocery_list_app/models/grocery_list.dart';
+import 'package:grocery_list_app/pages/product_selector.dart';
 
 class ProductListView extends StatefulWidget {
   final GroceryList myList;
@@ -15,7 +18,11 @@ class _ProductListViewState extends State<ProductListView> {
     return ListView(
       shrinkWrap: true,
       children: <Widget>[
-        Text("List ${widget.myList.initDate}"),
+        Text(
+          _getStringFromDate(widget.myList.initDate),
+          style: Style.listTitleTextStyle,
+          textAlign: TextAlign.center,
+        ),
         ListView.builder(
           shrinkWrap: true,
           itemCount: widget.myList.productList.length,
@@ -24,13 +31,41 @@ class _ProductListViewState extends State<ProductListView> {
             // print(item);
             String pro = item['productName'];
             String mag = item['productMagnitude'];
-
-            return ListTile(
-              title: Text("$pro : $mag"),
-            );
+            return Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: Material(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.purpleAccent,
+                    elevation: 10,
+                    child: Container(
+                        decoration: BoxDecoration(
+                            // gradient: Styles.tileGradient,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Builder(
+                            builder: (context) => ListTile(
+                                  title: Text("$pro : $mag"),
+                                )))));
           },
-        )
+        ),
+        CupertinoButton(
+          onPressed: () {
+            _goToProductListView();
+          },
+          child: Text("ADD"),
+        ),
       ],
     );
+  }
+
+  String _getStringFromDate(DateTime date) {
+    return "${date.day} - ${date.month} - ${date.year}";
+  }
+
+  _goToProductListView() {
+    Navigator.push(
+        context,
+        CupertinoPageRoute(
+            builder: (context) => ProductSelector(widget.documentID)));
   }
 }
