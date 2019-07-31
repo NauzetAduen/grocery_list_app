5af5d33983966ec98dc2e94ef1bfbf7571fcea06
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-31 18:44:36 +0100

    ADD temp textstyle

--- lib/Style/style.dart
+++ lib/Style/style.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-class Style{
-  
+class Style {
   // Colors
   static final Color green = Color(0xff84E0A1);
 
+  static final TextStyle listTitleTextStyle =
+      TextStyle(fontWeight: FontWeight.bold, fontSize: 24);
 }
