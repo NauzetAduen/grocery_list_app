616b64ebe871e0846b4514a21df4d5b885799096
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-07-31 22:59:02 +0100

    ADD streamBuilder to productSelector

--- lib/pages/product_selector.dart
+++ lib/pages/product_selector.dart
@@ -1,9 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:grocery_list_app/components/leading_appbar.dart';
 
 class ProductSelector extends StatefulWidget {
   final String documentID;
+  final List<Map<String, dynamic>> productList;
 
-  ProductSelector(this.documentID);
+  ProductSelector(this.documentID, this.productList);
   @override
   _ProductSelectorState createState() => _ProductSelectorState();
 }
@@ -11,10 +14,24 @@ class ProductSelector extends StatefulWidget {
 class _ProductSelectorState extends State<ProductSelector> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(widget.documentID),
-      ),
-    );
+    return StreamBuilder<Object>(
+        stream: Firestore.instance
+            .collection("products")
+            .orderBy("used", descending: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+
+          QuerySnapshot snap = snapshot.data;
+
+          return Scaffold(
+              appBar: LeadingAppbar(Text("Add products")),
+              body: Center(
+                child: Text("ASDASD"),
+              ));
+        });
   }
 }
