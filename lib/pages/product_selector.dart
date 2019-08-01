477c10a29f8be721c25fd14db5741a1a15936100
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-08-01 18:44:32 +0100

    ADD color in Cards depending if added

--- lib/pages/product_selector.dart
+++ lib/pages/product_selector.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:grocery_list_app/components/leading_appbar.dart';
+import 'package:grocery_list_app/models/product.dart';
 
 class ProductSelector extends StatefulWidget {
   final String documentID;
@@ -29,9 +30,41 @@ class _ProductSelectorState extends State<ProductSelector> {
 
           return Scaffold(
               appBar: LeadingAppbar(Text("Add products")),
-              body: Center(
-                child: Text("ASDASD"),
+              body: GridView.builder(
+                itemCount: snap.documents.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3),
+                itemBuilder: (BuildContext context, int index) {
+                  Product product =
+                      Product.fromJson(snap.documents[index].data);
+
+                  return GestureDetector(
+                    // onTap: () => _showDialog(context, product),
+                    child: Card(
+                      color: _getColor(widget.productList, product.name),
+                      child: Center(
+                        child: Text(
+                          product.name,
+                          style: TextStyle(fontSize: 22),
+                        ),
+                      ),
+                    ),
+                  );
+                },
               ));
         });
+  }
+
+  Color _getColor(List<Map<String, dynamic>> list, String productName) {
+    bool found = false;
+    list.forEach((doc) {
+      if (doc['productName'].toString().toLowerCase() ==
+          productName.toLowerCase()) {
+        found = true;
+        return;
+      }
+    });
+    if (found) return Colors.grey;
+    return Colors.red;
   }
 }
