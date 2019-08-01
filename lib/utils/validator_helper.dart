3f6925501a949a7b07b7621be404a1d457dba946
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-08-01 20:45:25 +0100

    ADD editingMag validation

--- lib/utils/validator_helper.dart
+++ lib/utils/validator_helper.dart
@@ -2,4 +2,8 @@ class ValidatorHelper {
   static String newProductValidator(String value) {
     return value.isEmpty ? "Please add a name" : null;
   }
+
+  static String editingMagnitudeValidator(String value) {
+    return value.isEmpty ? "Please add a magnitude" : null;
+  }
 }
