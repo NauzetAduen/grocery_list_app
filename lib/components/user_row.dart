ee1ada9ceefc491dd453beda8232a35676e6d1ed
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-08-07 05:49:48 +0100

    ADD IconButton to add new users to groups

--- lib/components/user_row.dart
+++ lib/components/user_row.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class UserRow extends StatelessWidget {
   final List<String> users;
@@ -6,22 +7,40 @@ class UserRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 75,
-      child: ListView.builder(
-        physics: BouncingScrollPhysics(),
-        scrollDirection: Axis.horizontal,
-        itemCount: users.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[CircleAvatar(), Text(users[index])],
+    return Stack(
+      children: <Widget>[
+        Container(
+            height: 75,
+            child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[CircleAvatar(), Text(users[index])],
+                    ),
+                  );
+                })),
+        Positioned(
+          top: 5.0,
+          bottom: 5.0,
+          right: 5.0,
+          child: CircleAvatar(
+            child: IconButton(
+              icon: Icon(
+                FontAwesomeIcons.plus,
+                size: 20,
+              ),
+              onPressed: () {
+                print("ADD NEW TO FUCKING GRUP");
+              },
             ),
-          );
-        },
-      ),
+          ),
+        )
+      ],
     );
   }
 }
