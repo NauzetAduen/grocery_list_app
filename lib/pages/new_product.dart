fdf9d18ea75a9678864d11d49b942f7cc2107ce5
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-08-09 03:44:50 +0100

    ADD icon dropdown

--- lib/pages/new_product.dart
+++ lib/pages/new_product.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gradient_widgets/gradient_widgets.dart';
 import 'package:grocery_list_app/Style/style.dart';
 import 'package:grocery_list_app/components/horizontal_separator.dart';
@@ -17,6 +18,13 @@ class _NewProductState extends State<NewProduct> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _controller = TextEditingController();
   var _scaffoldKey = new GlobalKey<ScaffoldState>();
+  String _icon;
+  @override
+  void initState() {
+    super.initState();
+    _icon = "Fruit";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,28 +37,52 @@ class _NewProductState extends State<NewProduct> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40),
-                  child: Row(
-                    children: <Widget>[
-                      Flexible(
-                        flex: 4,
-                        child: TextFormField(
-                          controller: _controller,
-                          validator: ValidatorHelper.newProductValidator,
-                        ),
-                      ),
-                      HorizontalSeparator(
-                        width: 30,
-                      ),
-                      GradientButton(
-                          child: Icon(Icons.check),
-                          callback: () => _validateNewProduct(),
-                          gradient: LinearGradient(
-                            colors: [Colors.green, Style.green],
-                          ))
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: TextFormField(
+                    controller: _controller,
+                    validator: ValidatorHelper.newProductValidator,
+                    decoration: InputDecoration(
+                        hintText: "Write a product name",
+                        prefixIcon: Icon(Icons.search),
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.cancel),
+                          onPressed: () {
+                            _controller.clear();
+                          },
+                        )),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: DropdownButtonFormField<String>(
+                    value: _icon,
+                    onChanged: (newValue) {
+                      setState(() {
+                        _icon = newValue;
+                      });
+                    },
+                    items: <DropdownMenuItem<String>>[
+                      DropdownMenuItem(
+                          value: "Fruit",
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Icon(FontAwesomeIcons.cloudMeatball),
+                          )),
+                      // DropdownMenuItem(
+                      //     value: "apple",
+                      //     child: Icon(FontAwesomeIcons.whatsappSquare)),
+                      // DropdownMenuItem(
+                      //     value: "fastfood", child: Icon(Icons.fastfood)),
                     ],
                   ),
-                )
+                ),
+                GradientButton(
+                    child: Icon(Icons.check),
+                    callback: () => _validateNewProduct(),
+                    gradient: LinearGradient(
+                      colors: [Colors.green, Style.green],
+                    )),
+                Text("$_icon")
               ],
             ),
           ),
