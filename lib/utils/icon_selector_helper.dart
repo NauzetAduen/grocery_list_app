a788b8836dbb8256037da16869163e0c70092d65
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-08-09 07:03:34 +0100

    IMPROVE iconSelector

--- lib/utils/icon_selector_helper.dart
+++ lib/utils/icon_selector_helper.dart
@@ -3,11 +3,32 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class IconSelectorHelper {
   static List<DropdownMenuItem<String>> list = <DropdownMenuItem<String>>[
-    DropdownMenuItem(
-        value: "Default",
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[Icon(FontAwesomeIcons.deaf), Text("Default")],
-        )),
+    _buildDropDown("Default", Icon(FontAwesomeIcons.shoppingBag)),
+    _buildDropDown("Cake", Icon(Icons.cake)),
+    _buildDropDown("Fast Food", Icon(Icons.fastfood)),
+    _buildDropDown("Coffee", Icon(Icons.local_cafe)),
+    _buildDropDown("Medicine", Icon(Icons.local_hospital)),
+    _buildDropDown("Pizza", Icon(Icons.local_pizza)),
+    _buildDropDown("Bread", Icon(FontAwesomeIcons.breadSlice)),
+    _buildDropDown("Cheese", Icon(FontAwesomeIcons.cheese)),
   ];
+
+  static DropdownMenuItem<String> _buildDropDown(String value, Icon icon) {
+    return DropdownMenuItem<String>(
+      value: value,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: icon,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 25),
+            child: Text(value),
+          ),
+        ],
+      ),
+    );
+  }
 }
