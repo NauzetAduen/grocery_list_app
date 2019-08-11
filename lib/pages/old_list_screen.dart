47d4222b5154ed3fe94899dfae5a320b686c8e2c
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-08-11 05:52:14 +0100

    ADD new listview for older lists

--- lib/pages/old_list_screen.dart
+++ lib/pages/old_list_screen.dart
@@ -3,9 +3,7 @@ import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:grocery_list_app/components/custom_appbar.dart';
-import 'package:grocery_list_app/components/product_list_view.dart';
 import 'package:grocery_list_app/models/grocery_list.dart';
-import 'package:page_view_indicators/circle_page_indicator.dart';
 
 class OldListScreen extends StatefulWidget {
   @override
@@ -13,7 +11,6 @@ class OldListScreen extends StatefulWidget {
 }
 
 class _OldListScreenState extends State<OldListScreen> {
-  final _currentPageNotifier = ValueNotifier<int>(0);
   @override
   void initState() {
     super.initState();
@@ -22,13 +19,13 @@ class _OldListScreenState extends State<OldListScreen> {
   @override
   Widget build(BuildContext context) {
     String uid = "Nauzet";
-    return StreamBuilder<Object>(
+    return Scaffold(
+      appBar: CustomAppbar(Text("Old lists")),
+      body: StreamBuilder<Object>(
         stream: Firestore.instance
             .collection("lists")
             .where("users", arrayContains: uid)
             .where("active", isEqualTo: false)
-            //!require an index
-            // .orderBy("initDate", descending: true)
             .snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData)
@@ -37,42 +34,19 @@ class _OldListScreenState extends State<OldListScreen> {
             );
           QuerySnapshot data = snapshot.data;
           List<DocumentSnapshot> documents = data.documents;
-          PageController pageController = PageController();
-          return Scaffold(
-            appBar: CustomAppbar(
-              Text("Older lists"),
-            ),
-            body: Stack(children: <Widget>[
-              PageView.builder(
-                itemCount: documents.length,
-                itemBuilder: (context, index) {
-                  Map<String, dynamic> mapJson = documents[index].data;
-                  GroceryList myList =
-                      GroceryList.fromJson(json.decode(json.encode(mapJson)));
-                  return ProductListView(myList, documents[index].documentID);
-                },
-                controller: pageController,
-                onPageChanged: (index) {
-                  _currentPageNotifier.value = index;
-                },
-              ),
-              Positioned(
-                left: 0.0,
-                right: 0.0,
-                bottom: 0.0,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CirclePageIndicator(
-                    size: 10,
-                    selectedSize: 15,
-                    selectedDotColor: Colors.red,
-                    itemCount: documents.length,
-                    currentPageNotifier: _currentPageNotifier,
-                  ),
-                ),
-              )
-            ]),
+
+          return ListView.builder(
+            shrinkWrap: true,
+            itemCount: documents.length,
+            itemBuilder: (context, index) {
+              Map<String, dynamic> mapJson = documents[index].data;
+              GroceryList tempList =
+                  GroceryList.fromJson(json.decode(json.encode(mapJson)));
+              return Text("${tempList.title}");
+            },
           );
-        });
+        },
+      ),
+    );
   }
 }
