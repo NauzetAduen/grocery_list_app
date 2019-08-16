a7996341cc4bc61b58976e841fb95daba9ff1c30
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-08-16 15:46:07 +0100

    ADD basic buttons for phoneValidation

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_widgets/gradient_widgets.dart';
@@ -21,6 +22,7 @@ class _LoginScreenState extends State<LoginScreen> {
         appBar: CustomAppbar(Text("Login")),
         body: SingleChildScrollView(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               GradientButton(
                 child: Row(
@@ -47,6 +49,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 },
               ),
               GradientButton(
+                elevation: 10,
                 increaseWidthBy: 50,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -75,15 +78,10 @@ class _LoginScreenState extends State<LoginScreen> {
                             RaisedButton(
                               onPressed: () {
                                 setState(() {
+                                  // phoneNumber = "+34" + _controller.text;
                                   phoneNumber = _controller.text;
-                                  print(phoneNumber);
-                                  // FirebaseAuth.instance.verifyPhoneNumber(
-                                  //     codeAutoRetrievalTimeout: (val) {},
-                                  //     codeSent: (val, [array]) {},
-                                  //     timeout: Duration(seconds: 5),
-                                  //     verificationCompleted: (val) {},
-                                  //     verificationFailed: (val) {},
-                                  //     phoneNumber: _controller.text);
+                                  print("Phone --> $phoneNumber");
+                                  verifyPhone();
                                 });
                               },
                               child: Text(
@@ -99,5 +97,69 @@ class _LoginScreenState extends State<LoginScreen> {
             ],
           ),
         ));
+  }
+
+  Future<void> verifyPhone() async {
+    final PhoneCodeAutoRetrievalTimeout autoRetrieve = (String verId) {
+      this.verificationCode = verId;
+    };
+    final PhoneCodeSent smsCodeSent = (String verId, [int forceCodeResend]) {
+      this.verificationCode = verId;
+      smsCodeDialog(context).then((value) {
+        print("smscodeDialog--> signed in");
+      });
+    };
+    final PhoneVerificationCompleted verifiedSuccess =
+        (AuthCredential authCredential) {
+      print(authCredential);
+      // FirebaseAuth.instance.signInWithCredential(authCredential).then((result){
+      //   print(result);
+      // });
+    };
+    final PhoneVerificationFailed veriFailed = (AuthException e) {
+      print(e.message);
+    };
+
+    await FirebaseAuth.instance.verifyPhoneNumber(
+        codeAutoRetrievalTimeout: autoRetrieve,
+        codeSent: smsCodeSent,
+        timeout: Duration(seconds: 5),
+        verificationCompleted: verifiedSuccess,
+        verificationFailed: veriFailed,
+        phoneNumber: _controller.text);
+    print("awaited");
+  }
+
+  Future<bool> smsCodeDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            contentPadding: EdgeInsets.all(10),
+            title: Text("Enter sms code"),
+            content: TextField(
+              onChanged: (newValue) {
+                this.smsCode = newValue;
+              },
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Done"),
+                onPressed: () {
+                  FirebaseAuth.instance.currentUser().then((user) {
+                    if (user != null) {
+                      Navigator.of(context).pop();
+                      //pushhomepage
+                    } else {
+                      Navigator.of(context).pop();
+                      //13:56
+                    }
+                  });
+                },
+              )
+            ],
+          );
+        });
   }
 }
