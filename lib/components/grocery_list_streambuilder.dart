4b52007811e22b3234b3ac9805dec2e68cab7dfe
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-09-12 17:34:14 +0100

    ADD streambuilder widget for grocerylists

---
+++ lib/components/grocery_list_streambuilder.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:grocery_list_app/components/grocery_listTile.dart';
+import 'package:grocery_list_app/models/grocery_list.dart';
+
+class GroceryListStreamBuilder extends StatelessWidget {
+  final bool active;
+  final String userID;
+  const GroceryListStreamBuilder({this.active, this.userID});
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Object>(
+        stream: Firestore.instance
+            .collection("lists")
+            .where("users", arrayContains: userID)
+            .where("active", isEqualTo: active)
+            .snapshots(),
+        builder: (context, snapshot) {
+          QuerySnapshot data;
+          List<DocumentSnapshot> documents;
+          if (snapshot.hasData) {
+            data = snapshot.data;
+            documents = data.documents;
+          }
+          return ListView.builder(
+            shrinkWrap: true,
+            itemCount: documents.length,
+            itemBuilder: (BuildContext context, int index) {
+              GroceryList gl = GroceryList.fromJsonFull(documents[index].data);
+              return GroceryListTile(
+                  gl: gl, documentID: documents[index].documentID);
+            },
+          );
+        });
+  }
+}
