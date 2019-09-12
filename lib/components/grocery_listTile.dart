41bdd365fa1de7926cce8ca9c056ee3a5e72246d
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-09-12 10:04:14 +0100

    ADD material config

--- lib/components/grocery_listTile.dart
+++ lib/components/grocery_listTile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:grocery_list_app/models/grocery_list.dart';
+import 'package:grocery_list_app/utils/date_helper.dart';
 
 class GroceryListTile extends StatelessWidget {
   final GroceryList gl;
@@ -12,12 +13,21 @@ class GroceryListTile extends StatelessWidget {
     Color color = gl.active ? Colors.red : Colors.grey;
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
+      child: Material(
         color: color,
+        elevation: 15.0,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            side: BorderSide(color: Colors.black)),
         child: ListTile(
-            title: Text(gl.title),
-            trailing:
-                gl.active ? Text("") : Text(gl.finishDate.toIso8601String())),
+          title: Text(gl.title),
+          trailing: gl.active
+              ? null
+              : Text(DateHelper.getStringFromDate(gl.finishDate)),
+          onTap: () {
+            print(gl.title);
+          },
+        ),
       ),
     );
   }
