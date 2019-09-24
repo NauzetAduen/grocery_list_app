e267aa468954dff7d6a0988d808068065368c50b
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-09-24 01:04:32 +0100

    REMOVE unnecesary StreamBuilder

--- lib/pages/homepage_screen.dart
+++ lib/pages/homepage_screen.dart
@@ -23,68 +23,54 @@ class _HomePageScreenState extends State<HomePageScreen> {
   Widget build(BuildContext context) {
     userID = Provider.of<FirebaseUser>(context).uid;
 
-    return StreamBuilder<Object>(
-        stream: Firestore.instance
-            .collection("lists")
-            .where("users", arrayContains: userID)
-            // .where("active", isEqualTo: true)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData)
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          // QuerySnapshot data = snapshot.data;
-          // List<DocumentSnapshot> documents = data.documents;
-          return Scaffold(
-            drawer: Drawer(
-              child: ListView(
-                children: <Widget>[
-                  DrawerHeader(
-                    child: Text("Options"),
-                    decoration: BoxDecoration(color: Style.green),
-                  ),
-                  ListTile(
-                    title: Text("Item 1"),
-                  ),
-                  ListTile(
-                    title: Text("Item 2"),
-                  ),
-                  ListTile(
-                    title: Text("Item 3"),
-                  ),
-                ],
-              ),
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text("Options"),
+              decoration: BoxDecoration(color: Style.green),
             ),
-            appBar: CustomAppbar(
-              Text("Home"),
+            ListTile(
+              title: Text("Item 1"),
             ),
-            body: ListView(
-              shrinkWrap: true,
-              children: <Widget>[
-                Align(
-                  child: Text("active"),
-                  alignment: Alignment.center,
-                ),
-                GroceryListStreamBuilder(userID: userID, active: true),
-                Align(
-                  child: Text("Innactive"),
-                  alignment: Alignment.center,
-                ),
-                GroceryListStreamBuilder(userID: userID, active: false),
-              ],
+            ListTile(
+              title: Text("Item 2"),
             ),
-            floatingActionButton: FloatingActionButton(
-                onPressed: () {
-                  print("Pressed");
-                },
-                backgroundColor: Style.green,
-                child: Center(
-                    child: Text(
-                  "New group",
-                  textAlign: TextAlign.center,
-                ))),
-          );
-        });
+            ListTile(
+              title: Text("Item 3"),
+            ),
+          ],
+        ),
+      ),
+      appBar: CustomAppbar(
+        Text("Home"),
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Align(
+            child: Text("active"),
+            alignment: Alignment.center,
+          ),
+          GroceryListStreamBuilder(userID: userID, active: true),
+          Align(
+            child: Text("Innactive"),
+            alignment: Alignment.center,
+          ),
+          GroceryListStreamBuilder(userID: userID, active: false),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("Pressed");
+          },
+          backgroundColor: Style.green,
+          child: Center(
+              child: Text(
+            "New group",
+            textAlign: TextAlign.center,
+          ))),
+    );
   }
 }
