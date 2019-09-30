ad094ce45727f042e326228dd76b06a9f6dccf49
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-09-30 07:38:55 +0100

    ADD images and names to userRow

--- lib/components/user_row.dart
+++ lib/components/user_row.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:grocery_list_app/models/user.dart';
 
 class UserRow extends StatelessWidget {
   final List<String> users;
@@ -8,18 +10,36 @@ class UserRow extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 75,
-        child: ListView.builder(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            itemCount: users.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[CircleAvatar(), Text("User $index")],
-                ),
-              );
+        child: FutureBuilder<Object>(
+            future: Firestore.instance.collection("users").getDocuments(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return SizedBox();
+              QuerySnapshot querySnapshot = snapshot.data;
+              List<DocumentSnapshot> list = querySnapshot.documents;
+              List<User> groupUsers = [];
+              list.forEach((doc) {
+                User tempUser = User.fromJson(doc.data);
+                if (users.contains(tempUser.id)) groupUsers.add(tempUser);
+              });
+              return ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: groupUsers.length,
+                  itemBuilder: (context, index) {
+                    User user = groupUsers[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundImage: NetworkImage("${user.photoURL}"),
+                          ),
+                          Text("${user.username}")
+                        ],
+                      ),
+                    );
+                  });
             }));
   }
 }
