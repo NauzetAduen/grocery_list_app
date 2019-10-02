561a20bdc72448ea100469f7a9606478f9797f27
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-10-02 05:47:24 +0100

    ADD new colors and textstyles

--- lib/Style/style.dart
+++ lib/Style/style.dart
@@ -3,10 +3,29 @@ import 'package:flutter/material.dart';
 class Style {
   // Colors
   static final Color green = Color(0xff84E0A1);
+  static final Color darkBlue = Color(0xFF003049);
+  static final Color darkRed = Color(0xFFd62828);
+  static final Color darkYellow = Color(0xFFf77f00);
+  static final Color lightYellow = Color(0xFFfcbf49);
+  static final Color whiteYellow = Color(0xFFeae2b7);
 
   static final TextStyle listTitleTextStyle =
       TextStyle(fontWeight: FontWeight.bold, fontSize: 24);
 
   static final TextStyle currentUsersListTitle =
       TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Colors.blue);
+  static final TextStyle addphoneButtonTextStyle =
+      TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Colors.black);
+  static final TextStyle welcomeStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 25,
+    color: whiteYellow,
+  );
+  static final TextStyle welcomeBiggerStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 30,
+    color: whiteYellow,
+  );
+  static final TextStyle appbarStyle =
+      TextStyle(fontWeight: FontWeight.bold, fontSize: 30);
 }
