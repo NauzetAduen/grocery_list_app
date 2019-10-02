66cef60896cd6e9e47047748372107857b03b032
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-10-02 06:16:11 +0100

    add generit validator for empty strings

--- lib/utils/validator_helper.dart
+++ lib/utils/validator_helper.dart
@@ -6,4 +6,8 @@ class ValidatorHelper {
   static String editingMagnitudeValidator(String value) {
     return value.isEmpty ? "Please add a magnitude" : null;
   }
+
+  static String genericEmptyValidator(String value) {
+    return value.isEmpty ? "This can't be empty" : null;
+  }
 }
