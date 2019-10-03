411e22d6a3dcf244cf9f3a4845bb4d38dece066a
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-10-03 07:37:38 +0100

    ADD style and INCREASE height

--- lib/components/user_row.dart
+++ lib/components/user_row.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:grocery_list_app/Style/style.dart';
 import 'package:grocery_list_app/models/user.dart';
 
 class UserRow extends StatelessWidget {
@@ -9,7 +10,7 @@ class UserRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 75,
+        height: 80,
         child: FutureBuilder<Object>(
             future: Firestore.instance.collection("users").getDocuments(),
             builder: (context, snapshot) {
@@ -28,14 +29,21 @@ class UserRow extends StatelessWidget {
                   itemBuilder: (context, index) {
                     User user = groupUsers[index];
                     return Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: <Widget>[
                           CircleAvatar(
                             backgroundImage: NetworkImage("${user.photoURL}"),
                           ),
-                          Text("${user.username}")
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "${user.username}",
+                            style: Style.addPhoneTextFieldStyle,
+                            textAlign: TextAlign.center,
+                          )
                         ],
                       ),
                     );
