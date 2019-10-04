61f5c49bd784b55842e3951a9639368298e3a5da
Nitish Kumar Singh <nitishk72@gmail.com>
2019-10-04 17:04:18 +0530

    dual material app parent, grocery list app

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_list_app/grocery_list_app.dart';
 
-import 'Style/style.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  final String appName = "Grocery List App";
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: appName,
-      theme: ThemeData(primaryColor: Style.green),
-      home: const GroceryListApp(),
-    );
-  }
-}
+void main() => runApp(GroceryListApp());
