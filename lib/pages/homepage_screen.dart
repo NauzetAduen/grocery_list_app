1f54f7b3f8197582986c84e208eb807a76536e1f
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-10-05 08:04:30 +0100

    ADD stack with avatar in header

--- lib/pages/homepage_screen.dart
+++ lib/pages/homepage_screen.dart
@@ -1,10 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:grocery_list_app/Style/style.dart';
 import 'package:grocery_list_app/components/custom_appbar.dart';
 import 'package:grocery_list_app/components/grocery_list_streambuilder.dart';
+import 'package:grocery_list_app/models/user.dart';
 import 'package:grocery_list_app/pages/new_group.dart';
+import 'package:grocery_list_app/pages/new_product.dart';
 import 'package:provider/provider.dart';
 
 class HomePageScreen extends StatefulWidget {
@@ -22,26 +26,89 @@ class _HomePageScreenState extends State<HomePageScreen> {
   @override
   Widget build(BuildContext context) {
     userID = Provider.of<FirebaseUser>(context).uid;
-
+    User myUser;
     return Scaffold(
       backgroundColor: Style.darkBlue,
       drawer: Drawer(
         child: Container(
           color: Style.darkBlue,
           child: ListView(
+            shrinkWrap: true,
             children: <Widget>[
-              DrawerHeader(
-                child: Text("Options"),
-                decoration: BoxDecoration(color: Style.lightYellow),
+              FutureBuilder(
+                future: Firestore.instance.collection("users").getDocuments(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData)
+                    return DrawerHeader(
+                      child: CircularProgressIndicator(),
+                    );
+                  QuerySnapshot querySnapshot = snapshot.data;
+                  List<DocumentSnapshot> list = querySnapshot.documents;
+                  for (int i = 0; i < list.length; i++) {
+                    User user = User.fromJson(list[i].data);
+                    if (user.id == userID) {
+                      myUser = user;
+                      break;
+                    }
+                  }
+                  return DrawerHeader(
+                    child: Stack(
+                      children: <Widget>[
+                        Align(
+                          child: CircleAvatar(
+                            radius: 50,
+                            backgroundImage: NetworkImage(myUser.photoURL),
+                          ),
+                          alignment: Alignment.centerRight,
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft + Alignment(.1, 0),
+                          child: Text(
+                            myUser.username,
+                            style: Style.drawerUsername,
+                          ),
+                        )
+                      ],
+                    ),
+                    decoration: BoxDecoration(color: Style.lightYellow),
+                  );
+                },
               ),
               ListTile(
-                title: Text("Logout"),
+                title: Text(
+                  "Change your @Username",
+                  style: Style.drawerListTileTextStyle,
+                ),
               ),
               ListTile(
-                title: Text("check product list"),
+                title: Text(
+                  "New product",
+                  style: Style.drawerListTileTextStyle,
+                ),
+                onTap: () => Navigator.push(context,
+                    CupertinoPageRoute(builder: (context) => NewProduct())),
+              ),
+              Divider(
+                color: Style.whiteYellow,
+                thickness: 2,
               ),
               ListTile(
-                title: Text("Change username"),
+                title: Text(
+                  "New group",
+                  style: Style.drawerListTileTextStyle,
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  "Leave a group",
+                  style: Style.drawerListTileTextStyle,
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  "Invite someone to a group",
+                  style: Style.drawerListTileTextStyle,
+                ),
               ),
             ],
           ),
