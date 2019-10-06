68557376777bcf2b884383db800de1ddc3e67589
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-10-06 05:50:39 +0100

    FIX default test after merge

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:grocery_list_app/main.dart';
+import 'package:grocery_list_app/grocery_list_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(GroceryListApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
