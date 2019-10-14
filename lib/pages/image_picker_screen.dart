d29a01ce0a90d409c032867c6183ee9a43f1a40f
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-10-14 23:38:35 +0100

    ADD imagepicker functionality

--- lib/pages/image_picker_screen.dart
+++ lib/pages/image_picker_screen.dart
@@ -12,6 +12,11 @@ class ImagePickerScreen extends StatefulWidget {
 
 class _ImagePickerScreenState extends State<ImagePickerScreen> {
   File _imageFile;
+  Image defaultImage = Image.asset(
+    "assets/images/nophotodefault.jpg",
+    width: 200,
+    height: 200,
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,12 +25,71 @@ class _ImagePickerScreenState extends State<ImagePickerScreen> {
         "Add picture",
         style: Style.appbarStyle,
       )),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          _imageFile == null
+              ? Container(
+                  alignment: Alignment.center,
+                  child: defaultImage,
+                )
+              : Container(
+                  alignment: Alignment.center,
+                  width: 300,
+                  height: 300,
+                  child: Image.file(_imageFile),
+                ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                child: FlatButton(
+                  color: Style.darkYellow,
+                  child: Column(
+                    children: <Widget>[
+                      Icon(
+                        Icons.photo_camera,
+                        color: Style.darkBlue,
+                        size: 40,
+                      ),
+                      Text("Camera", style: Style.cameraButtonsTextStyle),
+                    ],
+                  ),
+                  onPressed: () {
+                    _pickImage(ImageSource.camera);
+                  },
+                ),
+              ),
+              FlatButton(
+                color: Style.lightYellow,
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.photo_library,
+                      color: Style.darkBlue,
+                      size: 40,
+                    ),
+                    Text("Gallery", style: Style.cameraButtonsTextStyle),
+                  ],
+                ),
+                onPressed: () {
+                  _pickImage(ImageSource.gallery);
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
   Future<void> _pickImage(ImageSource source) async {
     File selected = await ImagePicker.pickImage(source: source);
-    _imageFile = selected;
+    setState(() {
+      _imageFile = selected;
+    });
   }
 
   void _clear() {
