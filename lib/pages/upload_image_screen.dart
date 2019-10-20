4133f569f14160bedbee0457cb4f402c04e95ce5
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-10-21 00:33:22 +0100

    ADD documentID to uploadImage constructor

--- lib/pages/upload_image_screen.dart
+++ lib/pages/upload_image_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -7,11 +8,13 @@ import 'package:grocery_list_app/Style/style.dart';
 import 'package:grocery_list_app/components/leading_appbar.dart';
 import 'package:grocery_list_app/utils/storage_helper.dart';
 import 'package:provider/provider.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
 class UploadImageScreen extends StatefulWidget {
   final File file;
+  final String documentID;
 
-  UploadImageScreen(this.file);
+  UploadImageScreen(this.file, this.documentID);
   @override
   _UploadImageScreenState createState() => _UploadImageScreenState();
 }
@@ -23,7 +26,7 @@ class _UploadImageScreenState extends State<UploadImageScreen> {
   StorageUploadTask _uploadTask;
 
   void _startUpload() {
-    String filePath = 'images/$userID.png';
+    String filePath = '$userID.png';
     setState(() {
       _uploadTask = _storage.ref().child(filePath).putFile(widget.file);
     });
@@ -38,31 +41,51 @@ class _UploadImageScreenState extends State<UploadImageScreen> {
                 ? StreamBuilder<StorageTaskEvent>(
                     stream: _uploadTask.events,
                     builder: (context, snapshot) {
-                      var event = snapshot?.data?.snapshot;
+                      if (!snapshot.hasData) return SizedBox();
+                      StorageTaskSnapshot event = snapshot?.data?.snapshot;
+                      double progressValue =
+                          event.bytesTransferred / event.totalByteCount;
+                      if (_uploadTask.isComplete) {
+                        // Firestore.instance
+                        //     .runTransaction((Transaction transaction) async {
+                        //   DocumentReference reference = Firestore.instance
+                        //       .collection("users")
+                        //       .document(widget.documentID);
+                        //   DocumentSnapshot snapshot =
+                        //       await transaction.get(reference);
+                        //   await _storage.ref().getDownloadURL().then((newURL) {
+                        //     print(newURL);
+                        //     transaction.update(
+                        //         snapshot.reference, {"photoURL": newURL});
+                        //   }
+                        //   );
+                        // }).then((onValue) {
+                        //   Navigator.popUntil(context, (route) => route.isFirst);
+                        // });
+                      }
+
+                      return SleekCircularSlider(
+                        appearance: CircularSliderAppearance(),
+                        initialValue: progressValue,
+                      );
                     },
                   )
-                : Hero(
-                    tag: "uploadTag",
-                    child: FlatButton(
-                      color: Style.darkRed,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Icon(
-                            Icons.cloud_upload,
-                            size: 40,
-                            color: Style.darkBlue,
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text("Upload", style: Style.cameraButtonsTextStyle),
-                        ],
-                      ),
-                      onPressed: () {
-                        _startUpload();
-                      },
+                : FlatButton(
+                    color: Style.darkRed,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(
+                          Icons.cloud_upload,
+                          size: 40,
+                          color: Style.darkBlue,
+                        ),
+                        Text("Upload", style: Style.cameraButtonsTextStyle),
+                      ],
                     ),
+                    onPressed: () {
+                      _startUpload();
+                    },
                   )),
         backgroundColor: Style.darkBlue,
         appBar: LeadingAppbar(
